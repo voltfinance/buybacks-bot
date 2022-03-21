9e3195a9735e9b4334e771584000b452457f6f9b
mul53 <mulengabowa53@gmail.com>
2022-03-21 20:35:39 +0200

    add error handler

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,19 @@ import VOLT_MAKER_ABI from './constants/abis/voltMaker.json'
 dotenv.config()
 
 async function buyback() {
-    const voltMaker = new ethers.Contract(VOLT_MAKER_ADDRESS, VOLT_MAKER_ABI)
-
-    let params: any = [[], []]
-
-    for (const pair of pairs) {
-        params = [[...params[0], pair[0]], [...params[1], pair[1]]]
+    try {
+        const voltMaker = new ethers.Contract(VOLT_MAKER_ADDRESS, VOLT_MAKER_ABI)
+    
+        let params: any = [[], []]
+    
+        for (const pair of pairs) {
+            params = [[...params[0], pair[0]], [...params[1], pair[1]]]
+        }
+    
+        await voltMaker.convertMultiple(params[0], params[1])
+    } catch (error) {
+        console.error(error)
     }
-
-    await voltMaker.convertMultiple(params[0], params[1])
 }
 
 console.log('Starting buybacks bot...')

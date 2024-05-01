93ef802b056e411070623465976636257392cf98
Mayank Mittal <msquare.pvt21@gmail.com>
2024-05-02 01:39:18 +0530

    Added Functionality of new fee contracts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { BigNumber, ethers } from 'ethers'
 
-import { pairs, VOLT, VOLT_MAKER_ADDRESS, VOLT_MAKER_V4_ADDRESS, xVOLT } from './constants'
-import VOLT_MAKER_ABI from './constants/abis/voltMaker.json'
-import ERC20_ABI from './constants/abis/erc20.json'
+import { FEE_MANAGER_ADDRESS } from './constants'
+import FEE_MANAGER_ABI from './constants/abis/FeeManager.json'
 import signer from './signer'
+import dotenv from 'dotenv'
+dotenv.config()
 
 function calculateGasMargin(value: BigNumber): BigNumber {
     return value.mul(BigNumber.from(10000).add(BigNumber.from(1000))).div(BigNumber.from(10000))
@@ -11,38 +12,40 @@ function calculateGasMargin(value: BigNumber): BigNumber {
 
 async function buyback() {
     try {
-        const voltMaker = new ethers.Contract(VOLT_MAKER_V4_ADDRESS, VOLT_MAKER_ABI, signer)
-    
-        for (const pair of pairs) {
+        const feeManager = new ethers.Contract(FEE_MANAGER_ADDRESS, FEE_MANAGER_ABI, signer)
+        for (let i = 0; i < 4; i++) {
             try {
-                const estimateGas = await voltMaker.estimateGas.convert(pair[0], pair[1])
-                await voltMaker.convert(pair[0], pair[1], { 
+                const estimateGas = await feeManager.estimateGas.collectFee(i)
+                await feeManager.collectFee(i, {
                     gasLimit: calculateGasMargin(estimateGas)
                 })
 
-                console.log(`V4 Converted pair for ${pair[0]}, ${pair[1]}`)
+                console.log(`Convert run for ${i} feeCollector`)
             } catch (error) {
-                console.error(`Failed to convert pair for ${pair[0]}, ${pair[1]} `, error)
+                console.error(`Failed to convert for ${i} feeCollector`, error)
             }
         }
 
     } catch (error) {
         console.error(error)
     }
+    await transferRewards();
 }
 
 async function transferRewards() {
+    const feeManager = new ethers.Contract(FEE_MANAGER_ADDRESS, FEE_MANAGER_ABI, signer)
     try {
-        const voltToken = new ethers.Contract(VOLT, ERC20_ABI, signer)
-
-        await voltToken.transfer(xVOLT, '27777000000000000000000')
-    } catch (error) {
+        const estimateGas = await feeManager.estimateGas.distribute()
+        await feeManager.distribute({
+            gasLimit: calculateGasMargin(estimateGas)
+        })
+        console.log('Rewards transferred')
+    }
+    catch (error) {
         console.error('Failed to transfer rewards', error)
     }
 }
-
 setInterval(buyback, Number(process.env.INTERVAL))
-setInterval(transferRewards, Number('86400000'))
 
 console.log('Starting buybacks and rewards bot...')
 
